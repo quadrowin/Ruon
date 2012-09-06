f3e880992426af2f6a3a5e44f2ded6c1852ac182
Goorus <goorus@list.ru>
2012-09-06 23:49:23 +0400

    + Тест для Injector/InjectorAbstract.php

--- Core/DependencyInjection/Injector/InjectorAbstract.php
+++ Core/DependencyInjection/Injector/InjectorAbstract.php
@@ -12,75 +12,76 @@ namespace Ruon\Core\DependencyInjection\Injector;
 abstract class InjectorAbstract
 {
 
-	/**
-	 * Источник сервисов
-	 *
-	 * @var \Ruon\Core\DependencyInjection\ContainerInterface
-	 */
-	protected $source;
+    /**
+     * Источник сервисов
+     *
+     * @var \Ruon\Core\DependencyInjection\ContainerInterface
+     */
+    protected $source;
 
-	/**
-	 * Возвращает название инъектируемого сервиса для свойства
-	 *
-	 * @param \ReflectionProperty $property
-	 * @return string
-	 */
-	public function getInjetionService(\ReflectionProperty $property)
-	{
-		$docComment = $property->getDocComment();
-		$match = preg_match(
-			'#@inject\s+([a-zA-Z_0-9\\\\]*)[\s\r\n]#',
-			$docComment,
-			$m
-		);
-		if (!$match) {
-			return null;
-		}
+    /**
+     * Возвращает название инъектируемого сервиса для свойства
+     *
+     * @param \ReflectionProperty $property
+     * @return string
+     */
+    public function getInjetionService(\ReflectionProperty $property)
+    {
+        $docComment = $property->getDocComment();
+        $m = null;
+        $match = preg_match(
+            '#@inject\s+([a-zA-Z_0-9\\\\]*)[\s\r\n]#',
+            $docComment,
+            $m
+        );
+        if (!$match) {
+            return null;
+        }
 
-		$serviceName = trim($m[1]);
-		if (!$serviceName) {
-			$match = preg_match(
-				'#@var\s+([a-zA-Z_0-9\\\\]*)[\s\r\n]#',
-				$docComment,
-				$m
-			);
-			if (!$match) {
-				return null;
-			}
+        $serviceName = trim($m[1]);
+        if (!$serviceName) {
+            $match = preg_match(
+                '#@var\s+([a-zA-Z_0-9\\\\]*)[\s\r\n]#',
+                $docComment,
+                $m
+            );
+            if (!$match) {
+                return null;
+            }
 
-			$serviceName = trim($m[1]);
-		}
+            $serviceName = trim($m[1]);
+        }
 
-		return $serviceName;
-	}
+        return $serviceName;
+    }
 
-	/**
-	 * Возвращает источник
-	 *
-	 * @return \Ruon\Core\DependencyInjection\ContainerInterface
-	 */
-	public function getSource()
-	{
-		return $this->source;
-	}
+    /**
+     * Возвращает источник
+     *
+     * @return \Ruon\Core\DependencyInjection\ContainerInterface
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
 
-	/**
-	 * Инициализирует зависимости в объекте
-	 *
-	 * @param object $object
-	 */
-	abstract public function inject($object);
+    /**
+     * Инициализирует зависимости в объекте
+     *
+     * @param object $object
+     */
+    abstract public function inject($object);
 
-	/**
-	 * Устанавливает источник сервисов для внедрения
-	 *
-	 * @param \Ruon\Core\DependencyInjection\ContainerInterface $source
-	 * @return $this|InjectorAbstract
-	 */
-	public function setSource($source)
-	{
-		$this->source = $source;
-		return $this;
-	}
+    /**
+     * Устанавливает источник сервисов для внедрения
+     *
+     * @param \Ruon\Core\DependencyInjection\ContainerInterface $source
+     * @return $this|InjectorAbstract
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+        return $this;
+    }
 
 }
